bfdaf0e00024d87af03226f45683b616b2ee0955
axsamli <ax.sam.li@zennioptical.com>
2024-07-18 17:57:22 +0800

    feat: save user

---
+++ service/user/save-user.ts
@@ -0,0 +1,12 @@
+import {createUser} from "~/repository/user/create-user";
+
+
+export async function saveUser(userDetail){
+    if (!userDetail){
+        console.log('User information cannot be empty')
+        return null;
+    }
+
+    const user =await createUser(userDetail);
+    return user ? user: null;
+}
